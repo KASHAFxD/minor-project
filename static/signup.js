7e3bb3dbdaa48e842a3a141f75d8cf1e30fb37dc
KASHAFxD <115388325+KASHAFxD@users.noreply.github.com>
2024-10-21 20:19:15 +0530

    Add files via upload

---
+++ static/signup.js
@@ -0,0 +1,34 @@
+
+function openForm() {
+    document.getElementById("popup").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("popup").style.display = "none";
+  }
+  
+
+
+
+
+function opensignup() {
+  document.getElementById("signuppg").style.display = "block";
+}
+
+function closesignup() {
+  document.getElementById("signuppg").style.display = "none";
+}
+
+
+
+
+
+
+
+function openlogin() {
+  document.getElementById("loginpg").style.display = "block";
+}
+
+function closelogin() {
+  document.getElementById("loginpg").style.display = "none";
+}
